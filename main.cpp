49350db1d4844801add79f0efc14d17305dc26a2
Donatien NAMBININTSOA <donatiennambinintsoa@gmail.com>
2022-01-06 09:09:05 +0300

    setup window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-#include "raylib.h";
+#include "raylib.h"
 
 int main() {
+  int WINDOW_WIDTH{800};
+  int WINDOW_HEIGHT{600};
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dapper dasher");
   return 0;
 }
