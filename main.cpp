19eedcdcb880736eb5dc0db9b8651db80702f744
Donatien NAMBININTSOA <donatiennambinintsoa@gmail.com>
2022-01-29 20:46:19 +0300

    add background

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "raylib.h"
+#include<cstdio>
 
-struct Element {
+struct Background {
+  Texture2D *texture;
+  Vector2 position;
+  Vector2 velocity;
+  float rotation;
+  float scale;
+  float updateTime;
+  float runningTime;
+};
+struct SpriteAnimation {
   int spriteCount;
   int currentSpriteFrameIndex;
   Texture2D *sprite;
@@ -11,9 +21,21 @@ struct Element {
   float runningTime;
 };
 
+void loopSpriteInTime(SpriteAnimation *element, float *deltaTime) {
+  (*element).runningTime += *deltaTime;
+  if ((*element).runningTime >= (*element).updateTime) {
+    (*element).runningTime = 0.0;
+    (*element).spriteSlice.x = (*element).currentSpriteFrameIndex * (*element).spriteSlice.width;
+    (*element).currentSpriteFrameIndex++;
+    if ((*element).currentSpriteFrameIndex > (*element).spriteCount - 1) {
+      (*element).currentSpriteFrameIndex = 0;
+    }
+  }
+}
+
 int main() {
   const int WINDOW_WIDTH{800};
-  const int WINDOW_HEIGHT{600};
+  const int WINDOW_HEIGHT{600}; 
 
   const int gravity{2'000};
   const int scarfyJumpVelocity{-1000};
@@ -22,32 +44,52 @@ int main() {
 
   Texture2D scarfySprite{LoadTexture("textures/scarfy.png")};
   Texture2D nebulaSprite{LoadTexture("textures/12_nebula_spritesheet.png")};
+  Texture2D farBuildings{LoadTexture("textures/far-buildings.png")};
+
+  /**
+   * @brief B A C K G R O U N D
+   * 
+   */
+  int farBuildingsBackgroundRepeatCount{2};
+  Background farBuildingsBackground[farBuildingsBackgroundRepeatCount]{};
+  for (int i = 0; i < farBuildingsBackgroundRepeatCount; i++){
+    farBuildingsBackground[i].texture = &farBuildings;
+    farBuildingsBackground[i].velocity.x = -80.0;
+    farBuildingsBackground[i].scale = 3.2;
+    farBuildingsBackground[i].rotation = 0.0;
+    farBuildingsBackground[i].position.x = i * ((*farBuildingsBackground[i].texture).width * farBuildingsBackground[i].scale);
+    farBuildingsBackground[i].position.y = 0;
+  }
+  
 
   /**
    * @brief N E B U L A
    * 
    */
-  Element nebula_1;
-  nebula_1.sprite = &nebulaSprite;
-  nebula_1.currentSpriteFrameIndex = 0;
-  nebula_1.spriteCount = 8;
-  nebula_1.spriteSlice.x = 0.0;
-  nebula_1.spriteSlice.y = 0.0;
-  nebula_1.spriteSlice.width = (*nebula_1.sprite).width / nebula_1.spriteCount;
-  nebula_1.spriteSlice.height = (*nebula_1.sprite).height / nebula_1.spriteCount;
-  nebula_1.position.x = float(WINDOW_WIDTH - 200);
-  nebula_1.position.y = float(WINDOW_HEIGHT - nebula_1.spriteSlice.height);
-  nebula_1.velocity.x = -200.0;
-  nebula_1.velocity.y = 0;
-  nebula_1.updateTime = 1.0 / 12.0;
-  nebula_1.runningTime = 0.0;
+  int numberOfNubela{25};
+  SpriteAnimation nebulae[numberOfNubela]{};
+  for (int i = 0; i < numberOfNubela; i++) {
+    nebulae[i].sprite = &nebulaSprite;
+    nebulae[i].currentSpriteFrameIndex = 0;
+    nebulae[i].spriteCount = 8;
+    nebulae[i].spriteSlice.x = 0.0;
+    nebulae[i].spriteSlice.y = 0.0;
+    nebulae[i].spriteSlice.width = (*nebulae[i].sprite).width / nebulae[i].spriteCount;
+    nebulae[i].spriteSlice.height = (*nebulae[i].sprite).height / nebulae[i].spriteCount;
+    nebulae[i].position.x = float(WINDOW_WIDTH + 100 * i);
+    nebulae[i].position.y = float(WINDOW_HEIGHT - nebulae[i].spriteSlice.height);
+    nebulae[i].velocity.x = -200.0;
+    nebulae[i].velocity.y = 0;
+    nebulae[i].updateTime = 1.0 / 12.0;
+    nebulae[i].runningTime = 0.0;
+  }
 
   /**
    * @brief S C A R F Y
    * 
    */
-  Element scarfy;
-  scarfy.sprite =&scarfySprite;
+  SpriteAnimation scarfy;
+  scarfy.sprite = &scarfySprite;
   scarfy.currentSpriteFrameIndex = 0;
   scarfy.spriteCount = 6;
   scarfy.spriteSlice.x = 0.0;
@@ -63,16 +105,30 @@ int main() {
   bool scarfyIsInTheAir{false};
 
   const int scarfyGroundPosition{WINDOW_HEIGHT - int(scarfy.spriteSlice.height)};
-  const float scarfyUpdateTime{1.0 / 12.0};
-  float scarfyRunningTime{};
 
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-    const float deltaTime{GetFrameTime()};
+    float deltaTime{GetFrameTime()};
 
     BeginDrawing();
     ClearBackground(WHITE);
+
+    /**
+     * @brief D R A W   B A C K G R O U N D
+     * 
+     */
+    for (size_t i = 0; i < farBuildingsBackgroundRepeatCount; i++) {
+      farBuildingsBackground[i].position.x += farBuildingsBackground[i].velocity.x * deltaTime;
+      DrawTextureEx(
+        (*farBuildingsBackground[i].texture),
+        farBuildingsBackground[i].position,
+        farBuildingsBackground[i].rotation,
+        farBuildingsBackground[i].scale,
+        WHITE
+      );
+    }
+    
 
     /**
      * @brief A P P L Y   G R A V I T Y
@@ -94,49 +150,31 @@ int main() {
       scarfy.velocity.y += scarfyJumpVelocity;
     }
 
-    scarfy.position.y += scarfy.velocity.y * deltaTime;
-    nebula_1.position.x += nebula_1.velocity.x * deltaTime;
-
-    /**
-     * @brief N E B U L A   A N I M A T I O N
-     * 
-     */
-    nebula_1.runningTime += deltaTime;
-    if (nebula_1.runningTime >= nebula_1.updateTime) {
-      nebula_1.runningTime = 0.0;
-      nebula_1.spriteSlice.x = nebula_1.currentSpriteFrameIndex * nebula_1.spriteSlice.width;
-      nebula_1.currentSpriteFrameIndex++;
-      if (nebula_1.currentSpriteFrameIndex > nebula_1.spriteCount - 1) {
-        nebula_1.currentSpriteFrameIndex = 0;
-      }
-
-    }
-
     /**
      * @brief S C A R F Y   A N I M A T I O N
      * 
      */
+    scarfy.position.y += scarfy.velocity.y * deltaTime;
     if (scarfyIsInTheAir) {
       if (scarfy.spriteSlice.x != 0) {
         scarfy.spriteSlice.x = 0;
       }
     } else {
-      scarfyRunningTime += deltaTime;
-      if (scarfyRunningTime >= scarfyUpdateTime) {
-        scarfyRunningTime = 0.0;
-        scarfy.spriteSlice.x = scarfy.currentSpriteFrameIndex * scarfy.spriteSlice.width;
-        scarfy.currentSpriteFrameIndex++;
-        if (scarfy.currentSpriteFrameIndex > scarfy.spriteCount - 1) {
-          scarfy.currentSpriteFrameIndex = 0;
-        }
-      }
+      loopSpriteInTime(&scarfy, &deltaTime);
     }
 
     /**
-     * @brief D R A W   N E B U L A
+     * @brief N E B U L A   A N I M A T I O N    &     D R A W   N E B U L A
      * 
      */
-    DrawTextureRec((*nebula_1.sprite), nebula_1.spriteSlice, nebula_1.position, WHITE);
+    for (int i = 0; i < numberOfNubela; i++) {
+      nebulae[i].position.x += nebulae[i].velocity.x * deltaTime;
+      nebulae[i].runningTime += deltaTime;
+      if (nebulae[i].runningTime >= nebulae[i].updateTime) {
+        loopSpriteInTime(&nebulae[i], &deltaTime);
+      }
+      DrawTextureRec((*nebulae[i].sprite), nebulae[i].spriteSlice, nebulae[i].position, WHITE);
+    }
 
     /**
      * @brief D R A W   S C A R F Y
@@ -148,7 +186,9 @@ int main() {
 
   CloseWindow();
 
-  UnloadTexture((*scarfy.sprite));
-  UnloadTexture((*nebula_1.sprite));
+  UnloadTexture(scarfySprite);
+  UnloadTexture(nebulaSprite);
+
+  UnloadTexture(farBuildings);
   return 0;
 }
